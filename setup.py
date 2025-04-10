05cc5b72c9e425ee28cfdf194310ce007b8a105b
Andrey Isambaev <isandrey2008@yandex.ru>
2025-04-11 01:26:23 +0400

    v 1.01

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 import setuptools
 
 
+required = ['setuptools>=75.0.0']
+
 setuptools.setup(
     name='simple_logger',
     version='1.01',
     packages=setuptools.find_packages(
-        include=['simple_logger']
-    )
+        include=['neto_decorators']
+    ),
+    install_reqires=required,
+    python_requires='>=3.8'
 )
